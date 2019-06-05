ce0408ee8989df720a6a789e9986ab5fc1efb14e
Gilda Snow <gilda.snow@gmail.com>
2019-06-05 17:58:35 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,8 @@ def my_select(collection)
  n = 0 
  selection = []
  while n < collection.length 
- selection << yield(collection[n])
+ if yield(collection[n])
+   selection << collection
  n+=1 
  end
  selection
